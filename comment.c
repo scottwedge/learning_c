dcac2b10a2a032c1f76956a859d911d61bce34e2
Scott Wedge <scott.wedge@gmail.com>
2023-01-20 23:12:22 -0500

    Practice with single line and multi-line comments

---
+++ comment.c
@@ -0,0 +1,12 @@
+// Single line comment begins with '//'
+
+/*  single or multi line comment begins with '/* 
+    and ends with  */
+
+#include <stdio.h>
+
+int main()
+{
+    puts("Hello");
+    puts("there");
+}
